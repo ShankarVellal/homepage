7406698ce9a0b424f08d6db9ce84ccd57c622df9
Shankar Vellal <vidyashankar.mv@gmail.com>
2017-10-14 19:48:32 -0700

    Remove HTML stored in variable and previous handler

--- go/src/main/main.go
+++ go/src/main/main.go
@@ -9,14 +9,8 @@ import (
     "golang.org/x/crypto/acme/autocert"
 )
 
-func indexHandler (w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
-    fmt.Fprintf(w, html)
-}
-
 func main() {
 	log.Printf("starting")
-    
     go http.ListenAndServe(":80", http.HandlerFunc(redirect))
 
     certManager := autocert.Manager{
@@ -24,17 +18,13 @@ func main() {
         HostPolicy: autocert.HostWhitelist("shankarvellal.com", "www.shankarvellal.com"),
         Cache:      autocert.DirCache("certs"), //folder for storing certificates
     }
-
     server := &http.Server{
         Addr: ":443",
         TLSConfig: &tls.Config{
             GetCertificate: certManager.GetCertificate,
         },
     }
-
     fs := http.StripPrefix("/", http.FileServer(http.Dir("public")))
-
-	//http.HandleFunc("/", indexHandler)
     http.Handle("/", fs)
 
 	if err := server.ListenAndServeTLS("", ""); err != nil {
